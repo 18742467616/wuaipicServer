74595130d97c2d82e4739a614c6b82dd2147aa0b
Stefan <yinxl@tourage.cn>
2019-06-12 15:47:18 +0800

    文件上传接口

---
+++ src/main/java/com/wuaipic/controller/FileController.java
@@ -0,0 +1,24 @@
+package com.wuaipic.controller;
+
+import com.wuaipic.result.ResultEntity;
+import com.wuaipic.service.IpCountService;
+import com.wuaipic.utils.FileUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+@RestController
+public class FileController {
+        @CrossOrigin
+        @RequestMapping(value = "file",method = RequestMethod.POST,headers = "Accept=application/json")
+        public ResultEntity Fileupload(MultipartFile file,HttpServletRequest request){
+            ResultEntity resultEntity =ResultEntity.createBySuccessData(FileUtil.FileUpload(file));
+            return resultEntity;
+
+    }
+
+}
---
+++ src/main/java/com/wuaipic/utils/FileUtil.java
@@ -0,0 +1,27 @@
+package com.wuaipic.utils;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileUtil {
+
+    public static String FileUpload(MultipartFile file){
+        String path="/usr/local/apache2/htdocs/aimImages/";
+        String fileName = file.getOriginalFilename();//获取到上传文件的名字
+        String suffix = fileName.substring(fileName.lastIndexOf('.'));//获取文件后缀
+        String newfileName = UUIDUtil.getOneUUID()+suffix;//随机生成文件名
+        System.out.println(newfileName);
+        File dir = new File(path,newfileName);//在目录中创建文件目录
+        if(!dir.getParentFile().exists()){
+            dir.getParentFile().mkdirs();//创建文件
+        }
+        try {
+            file.transferTo(dir);//转换文件
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return newfileName;
+    }
+}
