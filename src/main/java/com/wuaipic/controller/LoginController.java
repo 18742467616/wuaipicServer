6c1ca06484f52022b5e00715a22905c43b426f02
Stefan <yinxl@tourage.cn>
2019-07-09 16:26:41 +0800

    拦截器

--- src/main/java/com/wuaipic/controller/LoginController.java
+++ src/main/java/com/wuaipic/controller/LoginController.java
@@ -5,7 +5,8 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 import java.util.Map;
@@ -16,24 +17,20 @@ public class LoginController {
     // 发送post请求，代替了RequestMapping（value="/user/login", method="post"）
     @PostMapping(value = "/login")
     // 对登录请求判断request的参数值，并存放在map中
-    public void login(@RequestParam("username") String username,
-                        @RequestParam("password") String password,
-                        Map<String, Object> map, HttpSession httpSession) {
-//        // 我们判断，如果登录名不是空，并且，密码是 123456 就登录成功（暂不涉及数据库）
-//        if (!StringUtils.isEmpty(username) && "111111".equals(password)) {
-//            httpSession.setAttribute("user",username);
-//            // 登录成功，就跳转到下一个页面
-////            return "redirect:index";
-//            return "welcome";
-//        } else {
-//            // 登录失败，刷新本登录页
-//            map.put("msg", "用户名密码错误");
-//            return "redirect:/";
-//        }
+    public String login(@RequestParam("username") String username,
+                              @RequestParam("password") String password,
+                              Map<String, Object> map, HttpSession httpSession) {
+        // 我们判断，如果登录名不是空，并且，密码是 123456 就登录成功（暂不涉及数据库）
+        if (!StringUtils.isEmpty(username) && "111111".equals(password)) {
+            httpSession.setAttribute("user",username);
+            // 登录成功，就跳转到下一个页面
+           return "redirect:/index";
+        } else {
+            // 登录失败，刷新本登录页
+            map.put("msg", "用户名密码错误");
+            return "redirect:/";
+        }
 
-
-
-//        return "redirect:aaaaa";
     }
 
     @GetMapping("/")
@@ -48,8 +45,8 @@ public class LoginController {
     }
 
 
-    @GetMapping("/aaaaa")
-    public String aaaaa() {
+    @GetMapping("/index")
+    public String welcome() {
         return "welcome";
     }
 
